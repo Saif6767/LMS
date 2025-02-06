430082788ad42dd8867b3ca57c525683437fa18d
Saif Ali Khan <151372458+Saif6767@users.noreply.github.com>
2025-02-06 11:05:23 +0530

    Update Signup.jsx

--- Frontend/src/component/Signup.jsx
+++ Frontend/src/component/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
             password: data.password,
         };
         await axios
-            .post("http://localhost:4001/user/signup", userInfo)
+            .post("https://lms-lbh0.onrender.com/user/signup", userInfo)
             .then((res) => {
                 console.log(res);
                 if (res.data) {
